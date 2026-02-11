0e5ed658a699c66eeee3839fb2d8ef585d758d68
Ahmet Abdullah <realahmetabdullah@gmail.com>
2026-02-11 17:49:35 +0300

    Refactor Todo component to handle todo removal; integrate useDispatch and update event handling

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,19 +1,24 @@
 import { IoIosRemoveCircleOutline } from "react-icons/io";
 import { FaEdit } from "react-icons/fa";
 import type { TodoType } from "../types/types";
+import { useDispatch } from "react-redux";
+import { removeTodoById } from "../redux/slices/todoSlice";
 
 interface TodoProps {
   todoProps: TodoType;
 }
 
 function Todo({ todoProps }: TodoProps) {
-  const { id, content } = todoProps;
-
+ const dispatch = useDispatch();
+    const { id, content } = todoProps;
+        const handleRemoveTodo= () =>{
+            dispatch(removeTodoById(id))
+        }
   return (
     <div className="todo">
       <div>{content}</div>
       <div>
-        <IoIosRemoveCircleOutline className="icons" />
+        <IoIosRemoveCircleOutline onClick={handleRemoveTodo} className="icons" />
         <FaEdit className="icons" style={{ marginRight: "8px" }} />
       </div>
     </div>
--- src/redux/slices/todoSlice.ts
+++ src/redux/slices/todoSlice.ts
@@ -1,18 +1,25 @@
-import { createSlice } from '@reduxjs/toolkit'
-import type { PayloadAction } from '@reduxjs/toolkit'
-import type { TodoInitialState, TodoType } from '../../types/types'
-const initialState : TodoInitialState = {
-todos : []
-}
-export const todoSlice = createSlice({
-    name: "todo",
-    initialState,
-    reducers:{
-        createTodo : (state: TodoInitialState,action: PayloadAction<TodoType>) =>{
-            state.todos  = [...state.todos, action.payload]
-        }
-    }
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+import type { TodoInitialState, TodoType } from "../../types/types";
 
-})
-export const { createTodo } = todoSlice.actions
-export default todoSlice.reducer
+const initialState: TodoInitialState = {
+  todos: [],
+};
+
+export const todoSlice = createSlice({
+  name: "todo",
+  initialState,
+  reducers: {
+    createTodo: (state, action: PayloadAction<TodoType>) => {
+      state.todos.push(action.payload);
+    },
+
+    removeTodoById: (state, action: PayloadAction<number>) => {
+      state.todos = state.todos.filter(
+        (todo: TodoType) => todo.id !== action.payload,
+      );
+    },
+  },
+});
+
+export const { createTodo, removeTodoById } = todoSlice.actions;
+export default todoSlice.reducer;
