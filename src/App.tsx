3e82d91ff876cd118568e7e62c8e36f075b25e93
Ahmet Abdullah <realahmetabdullah@gmail.com>
2026-02-11 17:15:03 +0300

    Implement Redux for Todo management; add todoSlice, store configuration, and integrate with components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./css/App.css"
 import TodoCreate from "./components/TodoCreate"
 import TodoList from "./components/TodoList"
+
 function App() {
   return (
     <div>
--- src/components/TodoCreate.tsx
+++ src/components/TodoCreate.tsx
@@ -1,8 +1,27 @@
+import React, { useState } from "react";
+import { useDispatch} from "react-redux"
+import type { TodoType } from "../types/types";
+import { createTodo } from "../redux/slices/todoSlice";
+
 function TodoCreate() {
+    const dispatch = useDispatch();
+    const [newTodo, setNewTodo] = useState("");
+    const handleCreateTodo = () =>{
+        if(newTodo.trim().length == 0){
+            alert("Enter To Do")
+            return;
+        }
+        const payload: TodoType = {
+            id: Math.floor(Math.random()* 999999),
+            content : newTodo,
+        }
+        dispatch(createTodo(payload));
+        setNewTodo("");
+    }
   return (
     <div className='todo-create'>
-      <input placeholder="Enter Todo..." className="todo-create-input" type='text' />
-      <button className="todo-create-button">Create</button>
+      <input onChange={(e:React.ChangeEvent<HTMLInputElement>) => setNewTodo(e.target.value)} value={newTodo} placeholder="Enter Todo..." className="todo-create-input" type='text' />
+      <button onClick={handleCreateTodo} className="todo-create-button">Create</button>
     </div>
   )
 }  
